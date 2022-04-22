eeb23df733176159c449b425a8049197c5b254a9
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-04-22 13:26:38 -0700

    Add toggling feature for menu list and add menu page

--- src/components/BarControl.js
+++ src/components/BarControl.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MenuDetail from './MenuDetail';
+import MenuList from './MenuList';
+import NewMenuForm from './NewMenuForm';
 
 class BarControl extends React.Component {
   constructor(props){
@@ -9,14 +10,31 @@ class BarControl extends React.Component {
       mainMenuList : [],
       selectedMenu : null
     };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick =()=>{
+    this.setState(prevState=>({
+      formVisibleOnPage : !prevState.formVisibleOnPage
+    }));
+  }
+
   render(){
     let currentlyVisibleState= null;
-    if(!this.state.formVisibleOnPage)
-    currentlyVisibleState =<MenuDetail/>
+    let buttonText = null;
+    if(!this.state.formVisibleOnPage){
+    currentlyVisibleState =<MenuList/>
+    buttonText ="Add new menu"
+    }
+    else if(this.state.formVisibleOnPage){
+      currentlyVisibleState =<NewMenuForm/>
+      buttonText ="Return to menu List"
+    }
+
     return(
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick = {this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
