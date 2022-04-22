230f74d4eb13540077ee0347a0dec0e813263752
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-04-22 10:30:38 -0700

    Add class component

---
+++ src/components/BarControl.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BarControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={};
+  }
+  render(){
+    return(
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default BarControl ;
