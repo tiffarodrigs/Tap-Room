fea5d1c9ffec5d22ac2509e29ca59c15d7c50406
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-04-22 09:53:08 -0700

    Add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
+function App(){
+  return(
+    <React.Fragment>
+      <Header/>
+    </React.Fragment>
+  );
+}
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>Tap Room</h1>
+      <hr></hr>
+    </React.Fragment>
+  );
+}
+export default Header;
